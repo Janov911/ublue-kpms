ee9bf479a558bfc583deff63d230c99a6b5ad90f
Janov911 <19763626+Janov911@users.noreply.github.com>
2025-08-07 19:27:35 +0200

    bazzite instal correct issues

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y stow tmux ansible asciiquarium autojump bat cmake cmatrix dbus-devel firejail git-crypt irssi konsole libxkbcommon-devel lolcat lsd neovim newsboat nextcloud-client pcre podman-compose podman-docker ranger tealdeer trash-cli wxGTK-devel.x86_64 zoxide btop  
+dnf5 install -y stow ansible asciiquarium autojump bat cmatrix dbus-devel firejail git-crypt irssi konsole lolcat lsd neovim newsboat nextcloud-client pcre podman-compose podman-docker ranger tealdeer trash-cli wxGTK-devel.x86_64 zoxide --skip-broken 
 
 # Use a COPR Example:
 #
